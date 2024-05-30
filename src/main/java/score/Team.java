1720087505fe94fbb5d0e9b7e0cb713dce14a2d1
daniel.iwaniec <daniel.iwaniec@meinarztapp.de>
2024-05-30 18:05:51 +0200

    team implementation

---
+++ src/main/java/score/Team.java
@@ -0,0 +1,26 @@
+package score;
+
+public class Team {
+    private final String name;
+    private Integer score;
+
+    public Team(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void setScore(Integer score) {
+        if (score < 0) {
+            throw new IllegalArgumentException("Score must be greater than or equal to zero");
+        }
+        this.score = score;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public String toString() {
+        return name + " " + score;
+    }
+}

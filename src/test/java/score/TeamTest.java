cc485fccd4979e06d292195f49c31e2e341a7dcc
daniel.iwaniec <daniel.iwaniec@meinarztapp.de>
2024-05-30 18:01:29 +0200

    test for a team with score

---
+++ src/test/java/score/TeamTest.java
@@ -0,0 +1,36 @@
+package score;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TeamTest {
+    @Test
+    void testCreatingTeam() {
+        Team team = new Team("Poland");
+
+        assertEquals("Poland 0", team.toString());
+    }
+
+    @Test
+    void testSettingScore() {
+        Team team = new Team("Poland");
+        team.setScore(2);
+
+        assertEquals(2, team.getScore());
+        assertEquals("Poland 2", team.toString());
+    }
+
+    @Test
+    void testSettingInvalidScore() {
+        Team team = new Team("Poland");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> team.setScore(-2));
+
+        String expectedMessage = "Score must be greater than or equal to zero";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
